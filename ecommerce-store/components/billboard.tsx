62e8b767973dd40e0e451da601f65615d18a0b47
Mike <mike2021us@gmail.com>
2023-11-15 14:54:12 +0800

    Complete product list page

---
+++ ecommerce-store/components/billboard.tsx
@@ -0,0 +1,24 @@
+import { Billboard as BillboardType } from "@/types";
+
+interface BillbaordProps {
+  data: BillboardType;
+}
+
+const Billboard: React.FC<BillbaordProps> = ({ data }) => {
+  return (
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+      <div
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+      >
+        <div className="h-full w-full flex flex-col justify-center text-center gap-y-8">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-full max-w-xs">
+            {data.label}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Billboard;
